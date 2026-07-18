2d96c0614859e7661e824ecf29ed44633bf53914
Sim Kaurd <simkaurd21@gmail.com>
2026-07-18 15:57:00 -0400

    Updated API_URL

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:5000'
+const API_URL = 'https://biology-flashcard-biorecall.onrender.com/'
 
 export async function registerUser(email, password) {
    const response = await fetch(`${API_URL}/register`, {
